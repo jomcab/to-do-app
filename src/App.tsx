ce70d6716526b6a6ed8f060e60e70b2c228f0b9d
jomercab <jomercabrera@gmail.com>
2022-10-16 18:08:28 +0800

    Adding todo using state and interface

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
 import NewTodo from './components/NewTodo';
 import TodoList from './components/TodoList';
+import { Todo } from './todo.model';
 
 function App() {
-  const todos = [{ id: 't1', text: 'Sample to-do 1'}];
+  const [todos, setTodos] = useState<Todo[]>([]);
 
   const todoAddHandler = (text: string) => {
-    console.log(text);
+    setTodos(prevTodos => [...prevTodos, {id: Math.random().toString(), text: text}]);
   }
 
   return (
