a0bd70277751e2ceacbf8f2ced0e190d54fe7b3c
jomercab <jomercabrera@gmail.com>
2022-10-16 17:21:25 +0800

    Todo component with props and types for props

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import TodoList from './components/TodoList';
 
 function App() {
+  const todos = [{ id: 't1', text: 'Sample to-do 1'}];
   return (
     <div className="App">
-      Hello World
+      <TodoList items={todos}/>
     </div>
   );
 }
